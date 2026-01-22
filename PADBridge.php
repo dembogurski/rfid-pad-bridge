73600c5d173322969f580cb83e38c73433f44900
Doglas <dembogurski@gmail.com>
2026-01-22 11:47:06 -0300

    Se agrego Access-Control-Allow-Origin *

--- PADBridge.php
+++ PADBridge.php
@@ -7,6 +7,9 @@
  * Fecha: 21-01-2026
  */
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header("Content-Type: application/json; charset=utf-8");
 
 // -------------------------------------------------
